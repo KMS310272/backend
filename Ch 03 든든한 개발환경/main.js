a7515eb5c20bda771e12bc2461babf50e1d62958
KMS310272 <bella.minsokim@gmail.com>
2022-07-13 23:15:36 +0900

    prettier 다시 설치 ,적용 완료

--- Ch 03 든든한 개발환경/main.js
+++ Ch 03 든든한 개발환경/main.js
@@ -1,4 +1,4 @@
 // Formatting, Linting
 //Formatting -> 조금 더 미적인것, Linting -> 좀 잘 지키면 좋은것들
 //Formatting: Prettier 설치 명령어(npm install --save-dev prettier)
-console.log('Hello, world!')
+console.log("Hello, world!")
